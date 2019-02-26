0b58a3a765edef4cbbebd64e4e38d73b2eb8112d
Unknown <paulwilliamfussell@gmail.com>
2019-02-26 10:59:41 -0800

    map config

--- src/app/tracker/tracker.page.ts
+++ src/app/tracker/tracker.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  ToastController,
+  Platform,
+  LoadingController
+} from '@ionic/angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
+
+
 @Component({
   selector: 'app-tracker',
   templateUrl: './tracker.page.html',
   styleUrls: ['./tracker.page.scss'],
 })
-export class TrackerPage implements OnInit {
+export class TrackerPage implements OnInit {  
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    private platform: Platform) { }
+
+  async ngOnInit() {
+    
   }
 
+ 
 }
